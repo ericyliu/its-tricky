869d855db79db5da051b9b99b2821895dc0bc0e9
Tanner Whyte <tannersw@gmail.com>
2015-04-27 21:56:59 -0400

    Simple game set up. no support for multiple players

---
+++ Assets/player.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour {
+
+  public float damageTime = 1;
+  public Color damageColor = Color.red;
+  public int maxHealth = 100;
+
+  private Rigidbody2D rigidBody;
+  private float damageTimeLeft;
+  private bool spacePressed;
+  private Text healthText;
+  private int health;
+
+  // Use this for initialization
+	void Start () {
+    rigidBody = GetComponent<Rigidbody2D>();
+    damageTimeLeft = damageTime;
+    healthText = gameObject.GetComponentInChildren<Text> ();
+    health = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    // restore original player color
+    damageTimeLeft -= Time.deltaTime;
+    if (damageTimeLeft < 0) {
+      damageTimeLeft = damageTime;
+      gameObject.GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    // input logic
+    if (Input.GetKeyDown ("space")) {
+      spacePressed = true;
+    }
+    if (Input.GetKeyUp ("space")) {
+      spacePressed = false;
+    }
+
+    // update health label
+    healthText.text = "" + health;
+	}
+
+  void FixedUpdate() {
+    if (spacePressed) {
+      health = Mathf.Max(health - 1, 0);
+    }
+  }
+
+  void OnTriggerEnter2D (Collider2D collidee) {
+    if (!spacePressed) {
+      playerHit();
+    }
+    Rigidbody2D collideeRigidBody = collidee.gameObject.GetComponent<Rigidbody2D> ();
+    collideeRigidBody.AddForce (Vector2.right * 100 + Vector2.up * 900);
+  }
+
+  void playerHit() {
+    gameObject.GetComponent<Renderer>().material.color = damageColor;
+    health = Mathf.Max(health - 10, 0);
+  }
+
+  void playerDead() {
+
+  }
+}
---
+++ Assets/rocketEmitter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rocketEmitter : MonoBehaviour
+{
+  public float timeToNextEmit = 0;
+  public Transform projectile;
+  private GameObject player;
+  // Use this for initialization
+  void Start ()
+  {
+    
+  }
+  
+  // Update is called once per frame
+  void Update ()
+  {
+    timeToNextEmit -= Time.deltaTime;
+    if (timeToNextEmit < 0) {
+      timeToNextEmit = 2;
+
+      Instantiate (projectile, new Vector3 (0, 6, 0), Quaternion.identity);
+    }
+  }
+
+  void FixedUpdate ()
+  {
+  }
+
+  void spawnProjectile ()
+  {
+
+  }
+ 
+}
