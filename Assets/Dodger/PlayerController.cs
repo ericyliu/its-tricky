130068b93a5020efba7b355de4fd3ef3af27a7cf
Tanner Whyte <tannersw@gmail.com>
2015-04-29 22:21:23 -0400

    Added support for automatic layout of n players

--- Assets/Dodger/PlayerController.cs
+++ Assets/Dodger/PlayerController.cs
@@ -4,15 +4,15 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
   public Transform Player;
-  public int playerObjectCenterOffsetX = 0;
-  public int playerObjectCenterOffsetY = 0;
+  public float playerObjectCenterOffsetX = 0;
+  public float playerObjectCenterOffsetY = 0;
   private bool spacePressed;
   private GameObject[] players;
 
   // Use this for initialization
   void Start ()
   {
-    createPlayers (1);
+    createPlayers (3);
     layoutPlayers ();
   }
   
@@ -49,10 +49,15 @@ public class PlayerController : MonoBehaviour
   void layoutPlayers ()
   {
     int numPlayers = players.Length;
-
     for (int i = 0; i < numPlayers; i++) {
       GameObject player = players [i];
-      player.transform.position = new Vector2 (0.0f, -1.89f);
+      player.transform.position = new Vector2 (getPlayerXPosition(i, numPlayers), playerObjectCenterOffsetY);
     }
+  }
+
+  float getPlayerXPosition(int playerNumber, int totalPlayers) {
+    float screenWidth = 10;
+    float position = screenWidth * (1.0f * playerNumber + 1) / (1.0f * totalPlayers + 1);
+    return position - screenWidth / 2.0f;
   }
 }
