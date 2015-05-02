f9cd6cd8f01b8bfe5bc738e85a42b68b7d100fd7
Tanner Whyte <twhyte@etsy.com>
2015-05-02 19:56:23 -0400

    Dodger game in a place that networking can almost be tested with it. server needs to be written

--- Assets/Dodger/PlayerController.cs
+++ Assets/Dodger/PlayerController.cs
@@ -1,6 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*
+ * structure of message
+ * message type: string
+ * data: whatever
+ */
+
 public class PlayerController : MonoBehaviour
 {
   public Transform Player;
@@ -8,12 +14,15 @@ public class PlayerController : MonoBehaviour
   public float playerObjectCenterOffsetY = 0;
   private bool spacePressed;
   private GameObject[] players;
+  private int playerNumber = 0;
 
   // Use this for initialization
   void Start ()
   {
     createPlayers (3);
     layoutPlayers ();
+    player player = getPlayer (playerNumber);
+    player.networkPlayer = false;
   }
   
   // Update is called once per frame
@@ -27,9 +36,11 @@ public class PlayerController : MonoBehaviour
       spacePressed = false;
     }
 
-    GameObject player = players [0];
-    player playerScript = player.GetComponentInChildren<player> ();
-    playerScript.protect = spacePressed;
+    sendControlUpdate (spacePressed);
+    // this is to make sure its still working locally. get rid of this once
+    // networking is actually working
+    DodgerMessage msg = createDogderControlMessage (spacePressed);
+    receiveControlMessage (msg);
   }
 
   GameObject createPlayer ()
@@ -60,4 +71,51 @@ public class PlayerController : MonoBehaviour
     float position = screenWidth * (1.0f * playerNumber + 1) / (1.0f * totalPlayers + 1);
     return position - screenWidth / 2.0f;
   }
+
+  player getPlayer(int n) {
+    GameObject player = players [n];
+    return player.GetComponentInChildren<player> ();
+  }
+
+  void sendControlUpdate(bool dodging) {
+    DodgerMessage msg = createDogderControlMessage (dodging);
+
+    System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(msg.GetType());
+    // do the actual sending here.
+  }
+
+  DodgerMessage createDogderControlMessage(bool dodging) {
+    DodgerUpdateMessage msgData = new DodgerUpdateMessage ();
+    msgData.dodging = dodging;
+    msgData.playerNumber = playerNumber;
+    msgData.health = getPlayer (playerNumber).health;
+    DodgerMessage msg = new DodgerMessage ();
+    msg.data = msgData;
+    msg.type = "control";
+    return msg;
+  }
+
+  void receiveControlMessage(DodgerMessage msg) {
+    if (msg.type == "control") {
+      DodgerUpdateMessage controlMsg = (DodgerUpdateMessage) msg.data;
+      if (controlMsg.playerNumber == playerNumber) {
+        Debug.LogError("got a control message for self. somethings wrong");
+      }
+
+      player playerScript = getPlayer(controlMsg.playerNumber);
+      playerScript.protect = spacePressed;
+      playerScript.health = controlMsg.health;
+    }
+  }
+}
+
+public class DodgerMessage {
+  public string type;
+  public object data;
+}
+
+public class DodgerUpdateMessage {
+  public bool dodging;
+  public int health;
+  public int playerNumber;
 }
--- Assets/Dodger/player.cs
+++ Assets/Dodger/player.cs
@@ -2,62 +2,74 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class player : MonoBehaviour {
+public class player : MonoBehaviour
+{
 
   public float damageTime = 1;
   public Color damageColor = Color.red;
   public int maxHealth = 100;
+  public float protectDamageScale = .2f;
   public bool protect;
-
+  public int health;
+  public bool networkPlayer = true;
   private Rigidbody2D rigidBody;
+
   private float damageTimeLeft;
   private Text healthText;
-  private int health;
+  private float floatHealth;
 
   // Use this for initialization
-	void Start () {
-    rigidBody = GetComponent<Rigidbody2D>();
+  void Start ()
+  {
+    rigidBody = GetComponent<Rigidbody2D> ();
     damageTimeLeft = damageTime;
     healthText = gameObject.GetComponentInChildren<Text> ();
-    health = maxHealth;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    floatHealth = maxHealth;
+  }
+  
+  // Update is called once per frame
+  void Update ()
+  {
+    // update health label
+    health = (int)Mathf.RoundToInt (floatHealth);
+    healthText.text = "" + health;
+  }
+
+  void FixedUpdate ()
+  {
+    if (protect) {
+      floatHealth = Mathf.Max (floatHealth - protectDamageScale, 0);
+    }
+
     // restore original player color
     damageTimeLeft -= Time.deltaTime;
     if (damageTimeLeft < 0) {
-      damageTimeLeft = damageTime;
-      gameObject.GetComponent<Renderer>().material.color = Color.white;
-    }
-
-    // update health label
-    healthText.text = "" + health;
-	}
-
-  void FixedUpdate() {
-    if (protect) {
-      health = Mathf.Max(health - 1, 0);
+      Color colorToUpdate = Color.white;
+      if (protect) {
+        colorToUpdate = Color.yellow;
+      }
+      gameObject.GetComponent<Renderer> ().material.color = colorToUpdate;
     }
   }
 
-  void OnTriggerEnter2D (Collider2D collidee) {
+  void OnTriggerEnter2D (Collider2D collidee)
+  {
     if (!protect) {
-      playerHit();
+      playerHit ();
     }
     GameObject collideeObject = collidee.gameObject;
     Rigidbody2D collideeRigidBody = collideeObject.GetComponent<Rigidbody2D> ();
-    collideeObject.GetComponent<Collider2D>().enabled = false;
+    collideeObject.GetComponent<Collider2D> ().enabled = false;
 
     collideeRigidBody.AddForce (Vector2.right * 100 + Vector2.up * 900);
   }
 
-  void playerHit() {
-    gameObject.GetComponent<Renderer>().material.color = damageColor;
-    health = Mathf.Max(health - 10, 0);
-  }
-
-  void playerDead() {
-
+  void playerHit ()
+  {
+    gameObject.GetComponent<Renderer> ().material.color = damageColor;
+    damageTimeLeft = damageTime;
+    if (!networkPlayer) {
+      floatHealth = Mathf.Max (floatHealth - 10, 0);
+    }
   }
 }
